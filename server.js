0e61b65fa861c18f586292783fdb16cbcf0c64af
LetMahomed <ldesousa@byupathway.edu>
2025-06-03 21:58:47 -0300

    lets see

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
@@ -10,25 +11,37 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const utilities = require("./utilities") // ✅ Added this line to bring utilities into scope
 
 /* ***********************
-  * View Engine and Templates
+ * View Engine and Templates
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
-  * Routes
+ * Routes
  *************************/
-
 app.use(static)
 
 // Index route 
 app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+  res.render("index", { title: "Home" })
+})
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
 })
 
 /* ***********************
